1ed199e1b0e8b2a15f2f6ba17aa0b20fb25eeab7
MoonAmon <misterofclans.mo@gmail.com>
2024-02-27 21:17:09 -0300

    add login, register and record page

--- website/views.py
+++ website/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
+from .forms import SignUpForm
+from .models import Record
 
 def home(request):
+    records = Record.objects.all()
     # Check to see if logging in
     if request.method == 'POST':
         username = request.POST['username']
@@ -19,10 +21,39 @@ def home(request):
 
         return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records':records})
 
 
 def logout_user(request):
     logout(request)
     messages.success(request, "You Have Been Logged Out...")
     return redirect('home')
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You Have Successfully Register")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+
+    return render(request, 'register.html', {'form':form})
+
+
+def customer_record(request, pkey):
+    if request.user.is_authenticated:
+        # Look up records
+        customer_record = Record.objects.get(id=pkey)
+        return render(request, 'record.html', {'customer_record': customer_record})
+    else:
+        messages.success(request, "You Must Be Logged")
+        return render(request, 'home.html', {})
